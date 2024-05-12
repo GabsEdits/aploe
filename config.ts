3a734c92c16aaac669e059a2c432daeffed4c89a
Gabriel Cozma <me@gabs.eu.org>
2024-05-12 22:37:15 +0300

    feat: Simplify the script

--- config.ts
+++ config.ts
@@ -1,23 +1,5 @@
-import 'fs';
-
 interface UserConfig {
-    buildFrom?: string;
+  buildFrom: string;
 }
 
-let userConfig: UserConfig = {};
-try {
-    userConfig = require('./aploe.config.json');
-} catch (error) {
-    console.log('No Aploe configuration file found. Using default configuration.');
-}
-
-const defaultConfig = {
-    buildFrom: 'today'
-};
-
-const config: UserConfig = {
-    ...defaultConfig,
-    ...userConfig
-};
-
-export default config;
+export default UserConfig;
