228173055ca827e1e0b53a214f2e5ba7ed4a85e7
Gabriel Cozma <me@gabs.eu.org>
2024-06-16 16:42:02 +0300

    feat: add format workflow and small changes to extra info
    Prepreing for the stable release, and to meet all the requirements

--- components/ExtraInfo.vue
+++ components/ExtraInfo.vue
@@ -1,5 +1,21 @@
 <template>
-  <small style="margin-right: 20%" class="hide-on-mobile quick-office">
+  <small class="hide-on-mobile quick-info">
     <slot></slot>
   </small>
 </template>
+
+<style lang="scss">
+.quick-info {
+  margin-top: 20%;
+}
+
+.hide-on-mobile {
+  @media (max-width: 768px) {
+    display: none;
+  }
+
+  @media (min-width: 769px) {
+    display: block;
+  }
+}
+</style>
