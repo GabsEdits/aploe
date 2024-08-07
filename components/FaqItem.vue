72318fafe983fe7a43d553582a8311a9425af60d
Gabriel Cozma <me@gabs.eu.org>
2024-08-07 18:27:18 +0300

    feat: add faq item component

---
+++ components/FaqItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <article>
+    <h1>{{ $t(title) }}</h1>
+    <summary>{{ $t(content) }}</summary>
+  </article>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  title: String,
+  content: String,
+});
+</script>
