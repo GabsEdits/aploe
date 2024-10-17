dc137b2ae9d6cfc70d035286a30b320b0ee161f8
Gabriel Cozma <me@gabs.eu.org>
2024-10-17 22:08:02 +0300

    cleanup: fix fonts

--- mod.ts
+++ mod.ts
@@ -1,3 +1,3 @@
-import "npm:inter-ui/inter-display.css";
-import "npm:inter-ui/inter.css";
-import "npm:non.geist/mono";
+import "inter-ui/inter-display.css";
+import "inter-ui/inter.css";
+import "non.geist/mono";
